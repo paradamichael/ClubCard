d2108acf8af3d9319e870660f92aeae90064337d
Your Name <you@example.com>
2025-12-08 21:02:11 -0600

    Trigger deployment - version update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom'
 import AppRoutes from './router'
 import Header from './components/Header'
 
+// Version: December 2025 - UI/UX improvements
 export default function App() {
   const location = useLocation()
   const hideHeader = ['/', '/login', '/signup'].includes(location.pathname)
